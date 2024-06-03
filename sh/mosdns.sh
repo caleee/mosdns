7166a82b9a318fe108c40b7be324a0ecab0e8041
Cao Lei <caolei@mail.com>
2024-06-03 22:31:05 +0800

    Update mosdns.sh

--- sh/mosdns.sh
+++ sh/mosdns.sh
@@ -1,191 +1,182 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Filename: mosdns.sh
+# Author: Cao Lei <caolei@mail.com>
+# Date: 2024/03/28 - 2024/06/01
+# Version: 1.1.0
+# Description: This script is used to initialize and update mosdns configuration and data
+# Usage: Run this script as root: chmod +x mosdns.sh && ./mosdns.sh
+# Note: Ensure that you understand every command's behaviour and be careful when identifying large files
+#
+# For crontab: 0 3 * * * /etc/mosdns/mosdns.sh >> /var/log/consumer-mosdns-script.log 2>&1
+#
 
-script_action=${1}
+LOG_FILE="/var/log/consumer-mosdns-script.log"
+MOSDNS_DIR="/etc/mosdns"
+BACKUP_DIR="/opt/backup/mosdns"
 
-logfile_path() (
-    configfile=$(uci -q get mosdns.config.configfile)
-    if [ "$configfile" = "/etc/mosdns/config.yaml" ]; then
-        uci -q get mosdns.config.logfile
-    else
-        [ ! -f /etc/mosdns/config_custom.yaml ] && exit 1
-        awk '/^log:/{f=1;next}f==1{if($0~/file:/){print;exit}if($0~/^[^ ]/)exit}' /etc/mosdns/config_custom.yaml | grep -Eo "/[^'\"]+"
+log() {
+    local status cmd message datetime script_name user
+
+    status="$1"
+    cmd="$2"
+    message="$3"
+    datetime=$(date '+%Y-%m-%dT%H:%M:%S.%6N%:z')
+    script_name=$(basename "$0")
+    user=$(whoami)
+
+    echo "${datetime} ${status} ${script_name} (${user}) CMD (${cmd}) MSG (${message})" >>"${LOG_FILE}"
+}
+
+check() {
+    if ! systemctl status mosdns >/dev/null 2>&1; then
+        log "ERROR" "systemctl status mosdns" "Service check failed"
+        exit 1
     fi
-)
 
-interface_dns() (
-    if [ "$(uci -q get mosdns.config.custom_local_dns)" = 1 ]; then
-        uci -q get mosdns.config.local_dns
-    else
-        peerdns=$(uci -q get network.wan.peerdns)
-        proto=$(uci -q get network.wan.proto)
-        if [ "$peerdns" = 0 ] || [ "$proto" = "static" ]; then
-            uci -q get network.wan.dns
-        else
-            interface_status=$(ubus call network.interface.wan status)
-            echo $interface_status | jsonfilter -e "@['dns-server'][0]"
-            echo $interface_status | jsonfilter -e "@['dns-server'][1]"
-        fi
-        [ $? -ne 0 ] && echo "119.29.29.29 223.5.5.5"
+    if ! dnslookup baidu.com 127.0.0.1:5353 >/dev/null 2>&1; then
+        log "ERROR" "dnslookup baidu.com 127.0.0.1:5353" "DNS check for baidu.com failed"
+        exit 1
     fi
-)
 
-get_adlist() (
-    adblock=$(uci -q get mosdns.config.adblock)
-    if [ "$adblock" = 1 ]; then
-        mkdir -p /etc/mosdns/rule/adlist
-        ad_source=$(uci -q get mosdns.config.ad_source)
-        for url in $ad_source;
-        do
-            if [ $(echo $url) = 'geosite.dat' ]; then
-                echo "        - \"/var/mosdns/geosite_category-ads-all.txt\""
-            elif echo "$url" | grep -Eq "^file://" ; then
-                echo "        - \"$(echo "$url" | sed 's/file:\/\///')\""
+    if ! dnslookup example.org 127.0.0.1:5353 >/dev/null 2>&1; then
+        log "ERROR" "dnslookup example.org 127.0.0.1:5353" "DNS check for example.org failed"
+        exit 1
+    fi
+}
+
+backup() {
+    local mosdns_files date backup_file temp_backup_dir dir_to_create
+
+    mosdns_files=("mosdns.sh" "config_custom.yaml" "config_sample.yaml" "config.yaml" "default.yaml" "rule/blocklist.txt" "rule/cloudflare-cidr.txt" "rule/ddnslist.txt" "rule/disable-ads.txt" "rule/geoip-only-cn-private_cn.txt" "rule/geosite_apple.txt" "rule/geosite_category-ads-all.txt" "rule/geosite_cn.txt" "rule/geosite_geolocation-!cn.txt" "rule/greylist.txt" "rule/hosts.txt" "rule/local-ptr.txt" "rule/redirect.txt" "rule/whitelist.txt")
+    date=$(date '+%Y%m%d')
+    backup_file="mosdns_${date}.tar.gz"
+    temp_backup_dir=$(mktemp -d)
+
+    mkdir -p "${MOSDNS_DIR}"
+    mkdir -p "${BACKUP_DIR}"
+
+    for file in "${mosdns_files[@]}"; do
+        dir_to_create=$(dirname "${temp_backup_dir}/mosdns/${file}")
+        mkdir -p "${dir_to_create}"
+        cp "${MOSDNS_DIR}/${file}" "${dir_to_create}/"
+    done
+
+    tar czf "${BACKUP_DIR}/${backup_file}" -C "${temp_backup_dir}/mosdns" .
+    log "INFO" "tar czf ${BACKUP_DIR}/${backup_file}" "Backup successful"
+
+    rm -rf "${temp_backup_dir}"
+
+    # Keep only the latest 3 backups
+    (cd "${BACKUP_DIR}" && ls -t | tail -n +4 | xargs -r rm --)
+}
+
+update() {
+    local TMP_DIR url file
+
+    TMP_DIR=$(mktemp -d)
+
+    pushd "${TMP_DIR}" >/dev/null || exit 1
+
+    url=$(curl -s https://api.github.com/repos/caleee/mosdns/releases/latest | grep -o "https://github.com/caleee/mosdns/releases/download/v.*.tar.gz")
+    if [ -z "$url" ]; then
+        log "ERROR" "curl -s https://api.github.com/repos/caleee/mosdns/releases/latest" "No download URL found"
+        return 1
+    fi
+
+    if ! curl --connect-timeout 5 -m 60 --ipv4 -kfsSLO "$url"; then
+        log "ERROR" "curl --connect-timeout 5 -m 60 --ipv4 -kfsSLO $url" "Download failed"
+        return 1
+    fi
+
+    file=$(basename "$url")
+
+    if ! tar xzf "$file" -C /; then
+        log "ERROR" "tar xzf $file -C /" "Unpacking failed"
+        return 1
+    fi
+
+    popd >/dev/null || exit 1
+
+    rm -rf "${TMP_DIR}"
+
+    log "INFO" "update" "Mosdns update successfully"
+}
+
+restart() {
+    log "INFO" "systemctl restart" "Start restarting the mosdns service."
+    for attempt in 1 2; do
+        if systemctl systemctl restart mosdns; then
+            log "INFO" "systemctl restart" "Mosdns service successfully restarted on attempt $attempt."
+            if systemctl is-active --quiet mosdns.service; then
+                log "INFO" "systemctl is-active" "Mosdns service is active and running."
+                return 0
             else
-                echo "        - \"/etc/mosdns/rule/adlist/$(basename $url)\""
-                [ ! -f "/etc/mosdns/rule/adlist/$(basename $url)" ] && touch /etc/mosdns/rule/adlist/$(basename $url)
+                log "INFO" "systemctl is-active" "Mosdns service failed to become active."
             fi
-        done
-    else
-        rm -rf /etc/mosdns/rule/adlist /etc/mosdns/rule/.ad_source
-        touch /var/disable-ads.txt
-        echo "        - \"/var/disable-ads.txt\""
-    fi
-)
-
-adlist_update() {
-    [ "$(uci -q get mosdns.config.adblock)" != 1 ] && exit 0
-    ad_source=$(uci -q get mosdns.config.ad_source)
-    AD_TMPDIR=$(mktemp -d) || exit 1
-    mirror=""
-    : > /etc/mosdns/rule/.ad_source
-    has_update=0
-    for url in $ad_source;
-    do
-        if [ "$url" != "geosite.dat" ] && [ $(echo "$url" | grep -c -E "^file://") -eq 0 ]; then
-            has_update=1
-            echo "$url" >> /etc/mosdns/rule/.ad_source
-            filename=$(basename $url)
-            if echo "$url" | grep -Eq "^https://raw.githubusercontent.com" ; then
-                [ -n "$(uci -q get mosdns.config.github_proxy)" ] && mirror="$(uci -q get mosdns.config.github_proxy)/"
+        else
+            log "ERROR" "systemctl restart" "Failed to restart mosdns.service on attempt $attempt."
+            if [ "$attempt" -eq 1 ]; then
+                echo "Waiting 5 seconds before next attempt..."
+                sleep 5
+            else
+                log "CRITICAL" "systemctl restart" "Mosdns service failed to restart after 2 attempts."
+                exit 1
             fi
-            echo -e "\e[1;32mDownloading $mirror$url\e[0m"
-            curl --connect-timeout 5 -m 90 --ipv4 -kfSLo "$AD_TMPDIR/$filename" "$mirror$url"
         fi
     done
-    if [ $? -ne 0 ]; then
-        echo -e "\e[1;31mRules download failed.\e[0m"
-        rm -rf "$AD_TMPDIR"
-        exit 1
-    else
-        [ $has_update -eq 1 ] && {
-            mkdir -p /etc/mosdns/rule/adlist
-            rm -rf /etc/mosdns/rule/adlist/*
-            \cp $AD_TMPDIR/* /etc/mosdns/rule/adlist
-        }
-    fi
-    rm -rf "$AD_TMPDIR"
+    log "INFO" "systemctl restart" "Mosdns service restarted successfully."
 }
 
-geodat_update() (
-    TMPDIR=$(mktemp -d) || exit 1
-    [ -n "$(uci -q get mosdns.config.github_proxy)" ] && mirror="$(uci -q get mosdns.config.github_proxy)/"
-    # geoip.dat - cn-private
-    echo -e "\e[1;32mDownloading "$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat\e[0m"
-    curl --connect-timeout 5 -m 60 --ipv4 -kfSLo "$TMPDIR/geoip.dat" ""$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat"
-    [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
-    # checksum - geoip.dat
-    echo -e "\e[1;32mDownloading "$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat.sha256sum\e[0m"
-    curl --connect-timeout 5 -m 10 --ipv4 -kfSLo "$TMPDIR/geoip.dat.sha256sum" ""$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat.sha256sum"
-    [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
-    if [ "$(sha256sum "$TMPDIR/geoip.dat" | awk '{print $1}')" != "$(cat "$TMPDIR/geoip.dat.sha256sum" | awk '{print $1}')" ]; then
-        echo -e "\e[1;31mgeoip.dat checksum error\e[0m"
-        rm -rf "$TMPDIR"
-        exit 1
+restore() {
+    local backup_file
+
+    backup_file=$(ls -t "${BACKUP_DIR}"/mosdns_*.tar.gz | head -1)
+
+    if [ -z "$backup_file" ]; then
+        log "ERROR" "ls -t ${BACKUP_DIR}/mosdns_*.tar.gz" "No backup file found to restore"
+        return 1
     fi
 
-    # geosite.dat
-    echo -e "\e[1;32mDownloading "$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat\e[0m"
-    curl --connect-timeout 5 -m 120 --ipv4 -kfSLo "$TMPDIR/geosite.dat" ""$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-    [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
-    # checksum - geosite.dat
-    echo -e "\e[1;32mDownloading "$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat.sha256sum\e[0m"
-    curl --connect-timeout 5 -m 10 --ipv4 -kfSLo "$TMPDIR/geosite.dat.sha256sum" ""$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat.sha256sum"
-    [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
-    if [ "$(sha256sum "$TMPDIR/geosite.dat" | awk '{print $1}')" != "$(cat "$TMPDIR/geosite.dat.sha256sum" | awk '{print $1}')" ]; then
-        echo -e "\e[1;31mgeosite.dat checksum error\e[0m"
-        rm -rf "$TMPDIR"
-        exit 1
+    if ! tar xzf "$backup_file" -C "${MOSDNS_DIR}"; then
+        log "ERROR" "tar xzf $backup_file -C ${MOSDNS_DIR}" "Restore failed"
+        return 1
     fi
-    rm -rf "$TMPDIR"/*.sha256sum
-    \cp -a "$TMPDIR"/* /usr/share/v2ray
-    rm -rf "$TMPDIR"
-)
 
-restart_service() {
-    /etc/init.d/mosdns restart
+    log "INFO" "tar xzf $backup_file -C ${MOSDNS_DIR}" "Restore successful"
 }
 
-flush_cache() {
-    curl -s 127.0.0.1:$(uci -q get mosdns.config.listen_port_api)/plugins/lazy_cache/flush || exit 1
-}
-
-v2dat_dump() {
-    # env
-    v2dat_dir=/usr/share/v2ray
-    adblock=$(uci -q get mosdns.config.adblock)
-    ad_source=$(uci -q get mosdns.config.ad_source)
-    configfile=$(uci -q get mosdns.config.configfile)
-    mkdir -p /var/mosdns
-    rm -f /var/mosdns/geo*.txt
-    if [ "$configfile" = "/etc/mosdns/config.yaml" ]; then
-        # default config
-        v2dat unpack geoip -o /var/mosdns -f cn $v2dat_dir/geoip.dat
-        v2dat unpack geosite -o /var/mosdns -f cn -f apple -f 'geolocation-!cn' $v2dat_dir/geosite.dat
-        [ "$adblock" -eq 1 ] && [ $(echo $ad_source | grep -c geosite.dat) -ge '1' ] && v2dat unpack geosite -o /var/mosdns -f category-ads-all $v2dat_dir/geosite.dat
-    else
-        # custom config
-        v2dat unpack geoip -o /var/mosdns -f cn $v2dat_dir/geoip.dat
-        v2dat unpack geosite -o /var/mosdns -f cn -f 'geolocation-!cn' $v2dat_dir/geosite.dat
-        geoip_tags=$(uci -q get mosdns.config.geoip_tags)
-        geosite_tags=$(uci -q get mosdns.config.geosite_tags)
-        [ -n "$geoip_tags" ] && v2dat unpack geoip -o /var/mosdns $(echo $geoip_tags | sed -r 's/\S+/-f &/g') $v2dat_dir/geoip.dat
-        [ -n "$geosite_tags" ] && v2dat unpack geosite -o /var/mosdns $(echo $geosite_tags | sed -r 's/\S+/-f &/g') $v2dat_dir/geosite.dat
+main() {
+    check
+    backup
+    if ! update; then
+        restore
     fi
+    restart
 }
 
-cloudflare_ip() {
-    uci -q get mosdns.config.cloudflare_ip
-}
-
-case $script_action in
-    "dns")
-        interface_dns
-    ;;
-    "adlist")
-        get_adlist
-    ;;
-    "geodata")
-        geodat_update && adlist_update && restart_service
-    ;;
-    "logfile")
-        logfile_path
-    ;;
-    "adlist_update")
-        adlist_update && [ "$has_update" -eq 1 ] && restart_service
-    ;;
-    "flush")
-        flush_cache
-    ;;
-    "v2dat_dump")
-        v2dat_dump
-    ;;
-    "cloudflare")
-        cloudflare_ip
-    ;;
-    "version")
-        mosdns version
-    ;;
+if [ $# -eq 0 ]; then
+    main
+else
+    case "$1" in
+    check)
+        check
+        ;;
+    backup)
+        backup
+        ;;
+    update)
+        update
+        ;;
+    restore)
+        restore
+        ;;
+    restart)
+        restart
+        ;;
     *)
-        exit 0
-    ;;
-esac
+        echo "Usage: $0 {check|backup|update|restore|restart}"
+        exit 1
+        ;;
+    esac
+fi
